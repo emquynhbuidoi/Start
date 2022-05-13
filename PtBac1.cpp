9021eaea3a96c9fef8700554c37d904868e385f6
Quang <quang2312tp@gmail.com>
2022-05-13 18:18:50 +0700

    Buoi 2 008

---
+++ PtBac1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float heSoA, heSoB;
+    cout << "Nhap he so a: ";
+    cin >> heSoA;
+    cout << "Nhap he so b: ";
+    cin >> heSoB;
+    cout << heSoA << "x - " << heSoB << " = 0" << endl;
+    cout << "x = " << heSoB / heSoA;
+
+    return 0;
+}
